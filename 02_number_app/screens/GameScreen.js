675ec773a866d2eadc9057d8b1fe4854c937c25c
Andrii Zilnyk <andrii.zilnyk@eleks.com>
2022-10-21 19:08:21 +0300

    mooved color to constant

--- 02_number_app/screens/GameScreen.js
+++ 02_number_app/screens/GameScreen.js
@@ -1,9 +1,10 @@
 import { Text, View, StyleSheet } from 'react-native';
+import Title from '../components/Title';
 
 function GameScreen() {
   return (
     <View style={styles.screen}>
-      <Text>Openent's Gues</Text>
+      <Title style={styles.title}>Openent's Guess</Title>
       <View>
         <Text>Higher or lower?</Text>
         <Text>+ -</Text>
@@ -19,7 +20,7 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 12,
-  }
+  },
 });
 
 export default GameScreen;
